f1ade767dbd2856d98f42d61ee0b74dd7fc597c6
djeriko <jerikosalivan@gmail.com>
2023-07-25 20:55:26 +0300

    Create project skeleton

---
+++ src/features/content/Content.tsx
@@ -0,0 +1,9 @@
+import './Content.scss'
+
+const Content = () => {
+  return (
+    <main className='content'>Привет Реакт</main>
+  )
+}
+
+export default Content 
---
+++ src/features/sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+import './Sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <aside className='sidebar'>
+      <ul className='sidebar__list'>
+        <li className='sidebar__list-item'>one</li>
+        <li className='sidebar__list-item'>two</li>
+        <li className='sidebar__list-item'>three</li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
